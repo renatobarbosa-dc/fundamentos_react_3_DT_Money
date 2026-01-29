3b678ab3485d96a2cf618e815b1c977eab40ef77
renatobarbosa-dc <renatobcarvalho21@gmail.com>
2026-01-29 12:38:13 -0400

    transaction table

--- 03-dt-money/src/pages/Transactions/index.tsx
+++ 03-dt-money/src/pages/Transactions/index.tsx
@@ -1,9 +1,39 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import { TransactionsTable, TransactionsContainer, PriceHighlight } from "./styles";
 
 export function Transactions() {
     return(
         <div>
             <Header />
+            <Summary/>
+
+            <TransactionsContainer>
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width='40%'>Desenvolvimento de Site</td>
+                            <td>
+                                <PriceHighlight variant="income">
+                                    R$ 12.000,00
+                                </PriceHighlight>
+                            </td>
+                            <td>Venda</td>
+                            <td>29/01/2026</td>
+                        </tr>
+                        <tr>
+                            <td>Violão</td>
+                            <td>
+                                <PriceHighlight variant="outcome">
+                                    -R$ 2.000,00
+                                </PriceHighlight>
+                            </td>
+                            <td>Lazer</td>
+                            <td>29/01/2026</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
         </div>
     )
 }
