83f0890fce162cb9d658a7ffea7576adc5206350
renatobarbosa-dc <renatobcarvalho21@gmail.com>
2026-02-02 12:01:56 -0400

    updating transactions to use api

--- 03-dt-money/src/pages/Transactions/index.tsx
+++ 03-dt-money/src/pages/Transactions/index.tsx
@@ -1,42 +1,63 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./Components/SearchForm";
-import { TransactionsTable, TransactionsContainer, PriceHighlight } from "./styles";
+import {
+  TransactionsTable,
+  TransactionsContainer,
+  PriceHighlight,
+} from "./styles";
+
+interface Transactions {
+  id: number;
+  description: string;
+  type: "income" | "outcome";
+  price: number;
+  category: string;
+  createdAt: string;
+}
 
 export function Transactions() {
-    return(
-        <div>
-            <Header />
-            <Summary/>
+  const [transactions, setTransactions] = useState<Transactions[]>([]);
 
-            <TransactionsContainer>
-                <SearchForm/>
-                
-                <TransactionsTable>
-                    <tbody>
-                        <tr>
-                            <td width='40%'>Desenvolvimento de Site</td>
-                            <td>
-                                <PriceHighlight variant="income">
-                                    R$ 12.000,00
-                                </PriceHighlight>
-                            </td>
-                            <td>Venda</td>
-                            <td>29/01/2026</td>
-                        </tr>
-                        <tr>
-                            <td>Violão</td>
-                            <td>
-                                <PriceHighlight variant="outcome">
-                                    -R$ 2.000,00
-                                </PriceHighlight>
-                            </td>
-                            <td>Lazer</td>
-                            <td>29/01/2026</td>
-                        </tr>
-                    </tbody>
-                </TransactionsTable>
-            </TransactionsContainer>
-        </div>
-    )
+  async function loadTransactions() {
+    const response = await fetch("http://localhost:3000/transactions");
+    const data = await response.json();
+
+    setTransactions(data);
+  }
+
+  useEffect(() => {
+    loadTransactions();
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <SearchForm />
+
+        <TransactionsTable>
+          <tbody>
+            {transactions.map(transaction => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="50%">{transaction.description}</td>
+                  <td>
+                    <PriceHighlight variant={transaction.type}>
+                      R$ {transaction.price}
+                    </PriceHighlight>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>{transaction.createdAt}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
+  );
 }
