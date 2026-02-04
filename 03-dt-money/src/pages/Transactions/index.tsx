6b2a9885b0f23aa35280dec2182c6dd560363648
renatobarbosa-dc <renatobcarvalho21@gmail.com>
2026-02-04 10:57:57 -0400

    formatting improved

--- 03-dt-money/src/pages/Transactions/index.tsx
+++ 03-dt-money/src/pages/Transactions/index.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./Components/SearchForm";
@@ -7,13 +7,11 @@ import {
   TransactionsContainer,
   PriceHighlight,
 } from "./styles";
-import { formatDistanceToNow } from "date-fns";
-import { ptBR } from "date-fns/locale";
 import { TransactionsContext } from "../../contexts/TransactionContext";
-
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 
 export function Transactions() {
-  const { transactions } = useContext(TransactionsContext)
+  const { transactions } = useContext(TransactionsContext);
 
   return (
     <div>
@@ -31,15 +29,13 @@ export function Transactions() {
                   <td width="50%">{transaction.description}</td>
                   <td>
                     <PriceHighlight variant={transaction.type}>
-                      R$ {transaction.price}
+                      {transaction.type === "outcome" && "- "}
+                      {priceFormatter.format(transaction.price)}
                     </PriceHighlight>
                   </td>
                   <td>{transaction.category}</td>
                   <td>
-                    {formatDistanceToNow(new Date(transaction.createdAt), {
-                      addSuffix: true,
-                      locale: ptBR,
-                    })}
+                    {dateFormatter.format(new Date(transaction.createdAt))}
                   </td>
                 </tr>
               );
