87b496bfd781ca539c4773fb8d42cc73edd41b08
renatobarbosa-dc <renatobcarvalho21@gmail.com>
2026-02-07 14:46:52 -0400

    search bar functionality ok

--- 03-dt-money/src/contexts/TransactionsProvider.tsx
+++ 03-dt-money/src/contexts/TransactionsProvider.tsx
@@ -12,6 +12,7 @@ interface Transaction {
 
 export interface TransactionContextType {
   transactions: Transaction[];
+  fetchTransactions: (query?: string) => Promise<void>;
 }
 
 interface TransactionProviderProps {
@@ -21,19 +22,28 @@ interface TransactionProviderProps {
 export function TransactionsProvider({ children }: TransactionProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
-  async function loadTransactions() {
-    const response = await fetch("http://localhost:3000/transactions");
+  async function fetchTransactions(query?: string) {
+    const url = new URL('http://localhost:3000/transactions')
+
+    if (query) {
+      url.searchParams.append('q', query);
+    }
+
+    const response = await fetch(url);
     const data = await response.json();
 
     setTransactions(data);
   }
 
   useEffect(() => {
-    loadTransactions();
+    fetchTransactions();
   }, []);
 
   return (
-    <TransactionsContext.Provider value={{ transactions }}>
+    <TransactionsContext.Provider value={{ 
+      transactions,
+      fetchTransactions,
+    }}>
       {children}
     </TransactionsContext.Provider>
   );
