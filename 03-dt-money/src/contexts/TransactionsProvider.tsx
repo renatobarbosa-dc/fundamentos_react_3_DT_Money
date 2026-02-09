fe38142ee3b0917745e528b639d7c9e4acdd444a
renatobarbosa-dc <renatobcarvalho21@gmail.com>
2026-02-09 13:10:33 -0400

    creating transactions using the modal

--- 03-dt-money/src/contexts/TransactionsProvider.tsx
+++ 03-dt-money/src/contexts/TransactionsProvider.tsx
@@ -11,9 +11,17 @@ interface Transaction {
   createdAt: string;
 }
 
+interface CreateTransactionInput {
+  description: string;
+  price: number;
+  category: string;
+  type: 'income' | 'outcome';
+}
+
 export interface TransactionContextType {
   transactions: Transaction[];
   fetchTransactions: (query?: string) => Promise<void>;
+  createTransaction: (data: CreateTransactionInput) => Promise<void>
 }
 
 interface TransactionProviderProps {
@@ -26,10 +34,26 @@ export function TransactionsProvider({ children }: TransactionProviderProps) {
   async function fetchTransactions(query?: string) {
     const response = await api.get('/transactions', {
       params: {
+        _sort: 'createdAt',
+        _order: 'desc',
         q: query
       }
     })
     setTransactions(response.data);
+  }
+  
+  async function createTransaction(data: CreateTransactionInput){
+    const { description, price, category, type  } = data;
+
+    const response = await api.post('/transactions', {
+      description,
+      price,
+      category,
+      type,
+      createdAt: new Date(),
+    })
+
+    setTransactions(state => [response.data, ...state])
   }
 
   useEffect(() => {
@@ -40,6 +64,7 @@ export function TransactionsProvider({ children }: TransactionProviderProps) {
     <TransactionsContext.Provider value={{ 
       transactions,
       fetchTransactions,
+      createTransaction,
     }}>
       {children}
     </TransactionsContext.Provider>
