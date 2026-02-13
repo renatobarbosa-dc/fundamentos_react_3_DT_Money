5c8f309080f3d9b94f68c49e525f77a813083db8
renatobarbosa-dc <renatobcarvalho21@gmail.com>
2026-02-12 21:48:05 -0400

    use context selector

--- 03-dt-money/src/contexts/TransactionContext.tsx
+++ 03-dt-money/src/contexts/TransactionContext.tsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext } from "use-context-selector";
 import type { TransactionContextType } from "./TransactionsProvider";
 
 export const TransactionsContext = createContext({} as TransactionContextType);
